eaae6747f636b28252ddf9d979e97fb7cc447943
Albert Cerezo <acerezollavero@gmail.com>
2016-11-07 08:38:51 +0100

    p5 commented

--- P5/control.erl
+++ P5/control.erl
@@ -1,19 +1,23 @@
 -module(control).
 -export([go/2, worker/2]).
 
+% Authors: Albert Cerezo & Eloy Gil
+
+% Token ring version 1, the control process creates all the
+% workers and kills all workers too
+
 go(N, M) -> 
-    Tokens = [random:uniform(M) || _ <- lists:seq(1, M)],
+    Tokens = createrandoms(M, M),
     io:format("~p~n",[Tokens]),
     [First|Rest] = generate(N),
     setring(First, [First|Rest]),
     First ! {token, M-1},
     control(Tokens, [First|Rest], []).
 
-
+% Control process
 control([], Workers, Assigned) ->
     io:format("~p~n",[lists:reverse(Assigned)]),
-    stopworkers(Workers),
-    stop;
+    stopworkers(Workers);
 control([First|Others], Workers, Assigned) ->
     receive 
         {Pid, eats} -> io:format("~p eats ~n",[Pid]),
@@ -21,7 +25,7 @@ control([First|Others], Workers, Assigned) ->
             control(Others, Workers, NewAssigned)
     end.
 
-
+% Worker process
 worker(Control, Next) -> 
     receive
         {next, Pid} -> worker(Control, Pid);
@@ -33,16 +37,21 @@ worker(Control, Next) ->
         stop -> true
     end.
 
+% Creates a list of Times numbers between 1 and M
+createrandoms(_, 0) -> [];
+createrandoms(M, Times) -> [random:uniform(M)|createrandoms(M, Times-1)].
 
+% Generates N workers
 generate(0) -> [];
 generate(N) -> [spawn(control, worker, [self(), []]) |  generate(N-1)].
 
+% Sets the next worker for each worker
 setring(First, [H|[]]) -> H ! {next, First};
 setring(First, [H|[S|T]]) -> H ! {next, S},
     setring(First, [S|T]).
 
-
-stopworkers([Last|[]]) -> Last ! stop;
+% Sends a message of stop to all the workers
+stopworkers([]) -> stop;
 stopworkers([First|Rest]) -> First ! stop,
     stopworkers(Rest).
 
--- P5/control2.erl
+++ P5/control2.erl
@@ -1,14 +1,20 @@
 -module(control2).
 -export([go/2, initializefirst/2, initialize/3]).
 
+% Authors: Albert Cerezo & Eloy Gil
+
+% Token ring version 2, the control process creates a
+% single worker and each worker creates its next worker,
+% the same things happens in order to kill them
+
 go(N, M) -> 
-    Tokens = [random:uniform(M) || _ <- lists:seq(1, M)],
+    Tokens = createrandoms(M, M),
     io:format("~p~n",[Tokens]),
     FirstWorker = spawn(control2, initializefirst, [N-1, self()]),
     FirstWorker ! {token, M-1},
     control(Tokens, FirstWorker, []).
 
-
+% Control process
 control([], FirstWorker, Assigned) ->
     io:format("~p~n",[lists:reverse(Assigned)]),
     FirstWorker ! stop;
@@ -19,16 +25,7 @@ control([First|Others], FirstWorker, Assigned) ->
             control(Others, FirstWorker, NewAssigned)
     end.
 
-
-initializefirst(0, Control) -> worker(Control, self());
-initializefirst(N, Control) -> Next = spawn(control2, initialize, [N-1, Control, self()]),
-    worker(Control, Next).
-
-initialize(0, Control, First) -> worker(Control, First);
-initialize(N, Control, _) -> Next = spawn(control2, initialize, [N-1, Control, self()]),
-    worker(Control, Next).
-
-
+% Worker process
 worker(Control, Next) -> 
     receive
         {token, 0} -> Control ! {self(), eats},
@@ -38,3 +35,17 @@ worker(Control, Next) ->
             worker(Control, Next);
         stop -> Next ! stop
     end.
+
+% Creates a list of Times numbers between 1 and M
+createrandoms(_, 0) -> [];
+createrandoms(M, Times) -> [random:uniform(M)|createrandoms(M, Times-1)].
+
+% Initialize for the first 
+initializefirst(0, Control) -> worker(Control, self());
+initializefirst(N, Control) -> Next = spawn(control2, initialize, [N-1, Control, self()]),
+    worker(Control, Next).
+
+% Initialize others
+initialize(0, Control, First) -> worker(Control, First);
+initialize(N, Control, _) -> Next = spawn(control2, initialize, [N-1, Control, self()]),
+    worker(Control, Next).
